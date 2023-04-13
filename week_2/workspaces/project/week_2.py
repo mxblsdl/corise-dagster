77573084c86e49605dd70bbff1b6ea97af5f27e2
Max Blasdel <maxblasdel@gmail.com>
2023-04-13 17:23:19 +0000

    progress on week 2

--- week_2/workspaces/project/week_2.py
+++ week_2/workspaces/project/week_2.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from typing import List
-
+from heapq import nlargest
 from dagster import (
     In,
     Nothing,
@@ -16,29 +16,44 @@ from workspaces.resources import mock_s3_resource, redis_resource, s3_resource
 from workspaces.types import Aggregation, Stock
 
 
-@op
-def get_s3_data():
-    pass
+@op(config_schema={"s3_key": String}, required_resource_keys={"s3"}, out={"s3_out": Out(dagster_type=List[Stock])})
+def get_s3_data(context: OpExecutionContext):
+    out = context.resources.s3.get_data(key_name=context.op_config["s3_key"])
+    return [Stock.from_list(o) for o in out]
 
 
-@op
-def process_data():
-    pass
+# Helper for nlargest sort
+def sortkey(stock: Stock) -> int:
+    return stock.high
 
 
-@op
-def put_redis_data():
-    pass
+@op(ins={"process_data": In(dagster_type=List[Stock])}, out={"out": Out(dagster_type=Aggregation)})
+def process_data(context, process_data):
+    stock = nlargest(1, process_data, key=sortkey)
+    return Aggregation(date=stock[0].date, high=stock[0].high)
 
 
-@op
-def put_s3_data():
-    pass
+@op(required_resource_keys={"redis"}, ins={"agg": In(dagster_type=Aggregation)}, out=Out(Nothing))
+def put_redis_data(context, agg):
+    context.resources.redis.put_data(name=agg.date, value=str(agg.high))
+
+
+@op(
+    required_resource_keys={"s3"},
+    ins={"agg": In(dagster_type=Aggregation)},
+    out=Out(Nothing),
+    description="Put aggregation data in S3 bucket",
+)
+def put_s3_data(context, agg):
+    context.resources.s3.put_data(key_name=agg.date, data=agg)
 
 
 @graph
 def machine_learning_graph():
-    pass
+    stocks = get_s3_data()
+    data = process_data(process_data=stocks)
+    put_redis_data(agg=data)
+    put_s3_data(agg=data)
 
 
 local = {
@@ -55,8 +70,10 @@ docker = {
 
 machine_learning_job_local = machine_learning_graph.to_job(
     name="machine_learning_job_local",
+    config=local,
+    resource_defs={"s3": ResourceDefinition.mock_resource(), "redis": ResourceDefinition.mock_resource()},
 )
 
 machine_learning_job_docker = machine_learning_graph.to_job(
-    name="machine_learning_job_docker",
+    name="machine_learning_job_docker", config=docker, resource_defs={"s3": s3_resource, "redis": redis_resource}
 )
